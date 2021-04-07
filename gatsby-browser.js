70e20a1c7475ee82cb87c2fc23295ddef4beb402
wildpow <element1028@gmail.com>
2021-04-07 16:13:55 -0700

    chore: added back fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,3 +4,7 @@
 // export const wrapRootElement = ({ element }) => (
 //   <StoreProvider>{element}</StoreProvider>
 // );
+import "@fontsource/roboto";
+import "@fontsource/roboto/900.css";
+import "@fontsource/roboto/900-italic.css";
+import "@fontsource/roboto-slab";
